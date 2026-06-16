d96c2719f79f3e2da7cac4f00065245e60ed8fa1
Korolev Ivan <wazewa73@gmail.com>
2026-06-16 16:38:41 +0400

    added field name and added methods equals and hashcode

--- src/main/java/academy/mediasoft/team/BankAccount.java
+++ src/main/java/academy/mediasoft/team/BankAccount.java
@@ -1,18 +1,22 @@
 package academy.mediasoft.team;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.Random;
 
 public class BankAccount {
     private String nameOwner;
     private int balance;
     private LocalDateTime createdDate;
     private boolean isBlocked;
+    private int number;
 
     public BankAccount(String nameOwner) {
         this.nameOwner = nameOwner;
         balance = 0;
         createdDate = LocalDateTime.now();
         isBlocked = false;
+        number = new Random().nextInt(10000000, 100000000);
     }
     public boolean deposit(int amount) {
         if(isValidAmount(amount)) {
@@ -61,10 +65,32 @@ public class BankAccount {
         sb.append("Дата создания счета: ");
         sb.append(createdDate);
         sb.append("\n\t");
+        sb.append("Номер счета: ");
+        sb.append(number);
+        sb.append("\n\t");
         sb.append("Счет заблокирован? ");
         sb.append(isBlocked);
         sb.append("\n");
 
         return sb.toString();
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if(obj == null || this.getClass() != obj.getClass()) {
+            return false;
+        }
+        BankAccount bankAccount = (BankAccount) obj;
+        return (this.nameOwner.equals(bankAccount.nameOwner)
+            && this.balance == bankAccount.balance
+            && this.createdDate.equals(bankAccount.createdDate)
+            && this.isBlocked == bankAccount.isBlocked
+            && this.number == bankAccount.number);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameOwner, balance, createdDate, isBlocked, number);
     }
 }
